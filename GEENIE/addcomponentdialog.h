b0394c0fae3f02bdc9bc948c5aa1ee6896060fe9
Manschu <manuel.spengler@tu-ilmenau.de>
2016-06-11 23:37:51 +0200

    Frontend Doku

--- GEENIE/addcomponentdialog.h
+++ GEENIE/addcomponentdialog.h
@@ -15,18 +15,47 @@ class AddComponentDialog : public QDialog
     Q_OBJECT
 
 public:
+    //! Constructor.
+    /*!
+      \param parent The parent widget.
+     */
     explicit AddComponentDialog(QWidget *parent = 0);
+    //! Deconstructor.
+    /*!
+     * The dialog is deleted here.
+     */
     ~AddComponentDialog();
 
+    //! Get component.
+    /*!
+     * \return Component.
+     */
     Component* component();
 
 private slots:
+    //! Slot for the Create-Button.
+    /*!
+      A component for the actual Entity will be created.
+     */
     void on_createBtn_clicked();
 
+    //! Slot for the Cancel-Button.
+    /*!
+      Cancels assigning a Component to the selected Entity.
+     */
     void on_cancelBtn_clicked();
 
 private:
+    //! UI Variable.
+    /*!
+      Private Variable stores the AddComponentDialog-UI.
+     */
     Ui::AddComponentDialog *ui;
+
+    //! Component Variable.
+    /*!
+      Private Variable stores the actual Component.
+     */
     Component* _component;
 };
 
--- GEENIE/addentitydialog.h
+++ GEENIE/addentitydialog.h
@@ -12,18 +12,47 @@ class AddEntityDialog : public QDialog
     Q_OBJECT
 
 public:
+    //! Constructor.
+    /*!
+      \param parent The parent widget.
+     */
     explicit AddEntityDialog(QWidget *parent = 0);
+    //! Deconstructor.
+    /*!
+     * The dialog is deleted here.
+     */
     ~AddEntityDialog();
 
+    //! Get Entity-Name.
+    /*!
+     * \return A QString of the current Entity-Name.
+     */
     QString name(){return _name;}
 
 private slots:
+    //! Slot for the Create-Button.
+    /*!
+      If the selected Name of the new Entity is not empty, it will be created.
+     */
     void on_pushButton_clicked();
 
+    //! Slot for the Cancel-Button.
+    /*!
+      Will be called when the Cancel-Button is clicked. Aborts the Creation of a new Entity.
+     */
     void on_pushButton_2_clicked();
 
 private:
+    //! Name Variable.
+    /*!
+      Stores the private Name of the Entity. Is a QString.
+     */
     QString _name;
+
+    //! UI Variable.
+    /*!
+      Stores the AddEntityDialog UI. Is Private.
+     */
     Ui::AddEntityDialog *ui;
 };
 
